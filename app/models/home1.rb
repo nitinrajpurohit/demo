f35a854c2144fa785b70807fdd9fe6b9d144fa33
nitinrajpurohit <rajpurohitnitin7@gmail.com>
2016-01-08 17:13:16 +0530

    first commit

---
+++ app/models/home1.rb
@@ -0,0 +1,3 @@
+class Home1 < ActiveRecord::Base
+  attr_accessible :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'e92b391e6d04143481f8cb3bf8f15385aba630bceb8fd6e5a485472297936b0c3395f22f64d9aaac13fcc3236f00400bd9247c0fdcf31d74df22af25c7004ac6'
